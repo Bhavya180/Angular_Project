3e591d0de7e7538fd9202d5f1c334c56fd3b71f4
Bhavya Mangla <bhavyamangla93@gmail.com>
2024-04-05 16:35:21 +0530

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { PricingComponent} from './components/pricing/pricing.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, AboutComponent, ClientsComponent, PricingComponent,TestimonialsComponent, GalleryComponent, NavComponent, ServicesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { PricingComponent} from './components/pricing/pricing.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ServicesComponent } from './components/services/services.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {title: "Admister Studios | Home", path:'home' , component: HeaderComponent},
+    {title: "Admister Studios | About", path:'about' , component: AboutComponent},
+    {title: "Admister Studios | Clients", path:'clients' , component: ClientsComponent},
+    {title: "Admister Studios | Pricing", path:'pricing' , component: PricingComponent},
+    {title: "Admister Studios | Testimonials", path:'testimonials' , component: TestimonialsComponent},
+    {title: "Admister Studios | Gallery", path:'gallery' , component: GalleryComponent},
+    {title: "Admister Studios | Services", path:'services' , component: ServicesComponent},
+    {title: "Admister Studios | Not Found", path:'**' , component: NotfoundComponent},
+
+];
